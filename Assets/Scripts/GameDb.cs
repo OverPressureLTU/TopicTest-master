0e41138f417a437902026016badb8e10414cb682
葉佳彥 <YJY200234@gmail.com>
2023-10-26 05:59:38 +0800

    20231026

---
+++ Assets/Scripts/GameDb.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameDb
+{
+    public static int hp;
+    public static int energy;
+    public static bool isBGMReady = false;
+    public static bool touchWaterhole = false;
+    public static bool touchSand = false;
+    public static bool canJump;
+    public static float musicVolum = 0.5f;
+    public static float soundVolum = 1f;
+}
